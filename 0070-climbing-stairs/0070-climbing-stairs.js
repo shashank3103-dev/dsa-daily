ccd889a459938239b37926e8c022da2ab0a351ee
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-08-18 14:20:10 +0530

    Time: 0 ms (100%), Space: 52.5 MB (93.42%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    if (n <= 2) {
+        return n;
+    }
+
+    let prev2 = 1;
+    let prev1 = 2;
+
+    for (let i = 3; i <= n; i++) {
+        let current = prev1 + prev2;
+
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    return prev1;
+};

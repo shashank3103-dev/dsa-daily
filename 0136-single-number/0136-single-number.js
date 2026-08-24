0fd24544b698ffd4d6fc05091549b0468e7da75c
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-08-24 14:14:59 +0530

    Time: 368 ms (5.2%), Space: 53.1 MB (99.46%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -2,10 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
-   let result = 0;
-   for (let i = 0; i<nums.length; i++){
-    result = result ^ nums[i]
-   } 
-   return result
+var singleNumber = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                count++;
+            }
+        }
+        if (count == 1) return nums[i]
+    }
 };

cadfdeb31bdea8230fbfca45d7bf492653599440
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-08-24 13:56:07 +0530

    Time: 1 ms (69.53%), Space: 56 MB (68.92%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n = nums.length; 
+    let totalSum = n*(n+1)/2;
+
+    let sum = 0;
+
+    for(let i = 0; i<n; i++){
+        sum = sum + nums[i]
+    }
+    return totalSum - sum;
+};

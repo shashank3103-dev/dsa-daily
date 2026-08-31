a94f3e497d8c824b6494474c6174df6984ab972a
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-08-31 14:43:50 +0530

    Time: 0 ms (100%), Space: 54.7 MB (27.41%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function (s) {
+    let count = 0;
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "L") {
+            count++;
+        } else {
+            count--;
+        }
+
+        if (count === 0) {
+            result++;
+        }
+    }
+    return result;
+};

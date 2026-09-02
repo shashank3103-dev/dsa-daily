ab4dda6df01f329fb9ef4ba64c47f22f31974f3b
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-09-02 11:45:40 +0530

    Time: 0 ms (100%), Space: 53.8 MB (60.07%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+
+        let match = true;
+
+        for (let j = 0; j < needle.length; j++) {
+
+            if (haystack[i + j] !== needle[j]) {
+                match = false;
+                break;
+            }
+        }
+
+        if (match) {
+            return i;
+        }
+    }
+
+    return -1;
+};

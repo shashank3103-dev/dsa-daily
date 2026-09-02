15b97a79cddd89dfaa661b1d8f50f4b894b111fd
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-09-02 17:00:49 +0530

    Time: 0 ms (100%), Space: 54.3 MB (24.19%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function (s) {
+    let absent = 0;
+    let late = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "A") {
+            absent++;
+        }
+        if (s[i] === "L") {
+            late++;
+        } else {
+            late = 0;
+        }
+        if (absent >= 2 || late >= 3) {
+            return false;
+        }
+    }
+    return true;
+};

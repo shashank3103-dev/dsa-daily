a7cdf8264c92fccf2a71391866d3b8953f96aa12
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-09-02 15:08:45 +0530

    Time: 8 ms (31.91%), Space: 56.9 MB (32.18%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function (moves) {
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i < moves.length; i++) {
+        if (moves[i] === "R") {
+            x++;
+        }
+        if (moves[i] === "L") {
+            x--;
+        }
+        if (moves[i] === "U") {
+            y++;
+        }
+        if (moves[i] === "D") {
+            y--;
+        }
+    }
+    return x === 0 && y === 0;
+};

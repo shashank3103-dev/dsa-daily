80850856647403b80c49e4f850f0f2b1279fdd0a
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-09-05 11:33:44 +0530

    Time: 1 ms (68.32%), Space: 54.1 MB (81.37%) - LeetHub

---
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function(nums) {
+    let count = 1;
+    let max = 1;
+    for(let i = 1; i<nums.length; i++){
+        if(nums[i] >nums[i -1]){
+            count++;
+        }else{
+            count = 1;
+        }
+        if(count > max){
+            max = count;
+        }
+    }
+    return max;
+};

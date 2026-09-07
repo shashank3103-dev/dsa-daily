cf57ed549e9d4f17b7c6ab77c72920a6e02bc760
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-09-07 17:45:18 +0530

    Time: 0 ms (100%), Space: 57.6 MB (10.17%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
@@ -2,16 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maximumCount = function(nums) {
+var maximumCount = function (nums) {
     let positive = 0;
     let negative = 0;
-    for(let i = 0; i<nums.length; i++){
-        if(nums[i] > 0){
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
             positive++;
         }
-        else if(nums[i] < 0){
+        else if (nums[i] < 0) {
             negative++;
         }
     }
-    return Math.max(positive, negative);
+    if (positive > negative) {
+        return positive;
+    } else {
+        return negative;
+    }
 };

2c80e2f450f915a35c600d8fd2640e911381470d
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-09-07 12:16:16 +0530

    Time: 0 ms (100%), Space: 55.9 MB (26.91%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -5,12 +5,10 @@
  */
 var isSubsequence = function (s, t) {
     let i = 0;
-    let j = 0;
-    while (i < s.length && j < t.length) {
+    for (let j = 0; j < t.length; j++) {
         if (s[i] === t[j]) {
             i++;
         }
-        j++;
     }
     return i === s.length;
 };

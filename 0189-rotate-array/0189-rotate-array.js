69b9d808c8e27266a66817a6f1c819e9495e6005
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-09-07 15:06:28 +0530

    Time: 8 ms (32.13%), Space: 95.3 MB (29.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -5,7 +5,16 @@
  */
 var rotate = function (nums, k) {
     k = k % nums.length;
-    let last = nums.slice(nums.length - k);
-    let first = nums.slice(0, nums.length - k);
-    nums.splice(0, nums.length, ...last, ...first);
+
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
 };
+function reverse(nums, left, right) {
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+
+        left++;
+        right--;
+    }
+}

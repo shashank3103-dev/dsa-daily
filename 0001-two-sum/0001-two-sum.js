afcec8798b5ca025ad16940314d2b9a34c13be9f
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-09-14 09:47:59 +0530

    Time: 33 ms (30.79%), Space: 54.7 MB (60.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,6 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
-};
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] === target - nums[i]) {
+                return [i, j];
+            }
+        }
+    }
+    // Return an empty array if no solution is found
+    return [];
+} 

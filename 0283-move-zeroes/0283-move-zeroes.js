34b14167d8e0becca422e861cec47c387f3a24f7
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2026-09-15 17:06:40 +0530

    Time: 3 ms (38.22%), Space: 58.4 MB (95.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,12 +2,16 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
 var moveZeroes = function(nums) {
-    let left = 0;
-    for(let right = 0; right<nums.length; right++){
-        if(nums[right] !== 0){
-            [nums[left], nums[right]] = [nums[right],nums[left]];
-            left++;
+    let j = 0;
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] !== 0){
+            [nums[j], nums[i]] = [nums[i],nums[j]];
+            j++;
         }
     }
 };
